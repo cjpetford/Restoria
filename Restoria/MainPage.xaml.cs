a1d2a992bd97030959ce1f59b525ea6ec0ff02f8
Jake <jake.hodson-tomokino@techtorium.ac.nz>
2024-08-27 11:07:24 +1200

    Added Privacy Policy page, sticky button element so 'Continue' is always in view

---
+++ Restoria/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace Restoria
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void PP_OkayButton_Clicked(object sender, EventArgs e)
+        {
+            // Code for closing page goes here
+            // 'Pops' current page, taking user back to previous page. I think. - Jake 27/08/2022 10:57AM
+            await Navigation.PopAsync();
+        }
+    }
+
+}
