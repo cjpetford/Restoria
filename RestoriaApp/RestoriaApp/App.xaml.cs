813d738e1f866ff7a9f1d28a033b7aa99cea7fb4
ArthurMoore3000 <arthurmoore3000@gmail.com>
2024-08-28 13:19:50 +1200

    Created it as a .MAUI project

---
+++ RestoriaApp/RestoriaApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace RestoriaApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ RestoriaApp/RestoriaApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace RestoriaApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
